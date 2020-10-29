5523f26bde60c37eb331fe80a934bca8e9d950df
pwitte <pwitte@microsoft.com>
2020-10-29 08:23:07 -0700

    update model

--- InnerEye/ML/configs/segmentation/SeismicSegmentation.py
+++ InnerEye/ML/configs/segmentation/SeismicSegmentation.py
@@ -16,6 +16,7 @@ from InnerEye.ML.utils.split_dataset import DatasetSplits
 from Tests.fixed_paths_for_tests import full_ml_test_data_path
 from pathlib import Path
 from InnerEye.ML.deep_learning_config import OptimizerType
+from InnerEye.ML.utils.ml_util import is_gpu_available
 
 class SeismicSegmentation(SegmentationModelBase):
     """
@@ -27,27 +28,31 @@ class SeismicSegmentation(SegmentationModelBase):
         super().__init__(
             # Data definition - in this section we define where to load the dataset from
             #local_dataset=full_ml_test_data_path(),
-            local_dataset=Path('/home/pwitte/SeismicSegmentationChallenge'),
-            #azure_dataset_id='parihaka_patch_size_128',
-            
+            #local_dataset=Path('/home/pwitte/SeismicSegmentationChallenge/val_data'),
+            #azure_dataset_id='parihaka_training_full',
+            azure_dataset_id='parihaka_patch_size_128',
+
             # Model definition - in this section we define what model to use and some related configurations
             architecture="UNet3D",
-            feature_channels=[2],
-            crop_size=(64, 64, 64),
+            feature_channels=[32],
+            crop_size=(128, 128, 128),
             image_channels=["image"],
             ground_truth_ids=fg_classes,
             class_weights=equally_weighted_classes(fg_classes, background_weight=0.02),
+            
             #mask_id="mask",
+            #test_crop_size=(256, 256, 256),
+            #inference_stride_size=(128, 128, 128),
 
             # Model training and testing - in this section we define configurations pertaining to the model
             # training loop (ie: batch size, how many epochs to train, number of epochs to save)
             # and testing (ie: how many epochs to test)
-            use_gpu=False,
-            num_dataload_workers=0,
+            use_gpu=is_gpu_available(),
+            num_dataload_workers=2,
             train_batch_size=4,
             inference_batch_size=1,
-            start_epoch=0,
-            num_epochs=100,
+            start_epoch=100,
+            num_epochs=200,
             save_start_epoch=20,
             save_step_epochs=20,
             test_start_epoch=50,
@@ -56,7 +61,7 @@ class SeismicSegmentation(SegmentationModelBase):
             use_mixed_precision=True,
 
             # Optimizer
-            loss_type=SegmentationLoss.SoftDice,
+            loss_type=SegmentationLoss.SoftDice#CrossEntropy,
             l_rate=1e-3,
             min_l_rate=1e-5,
             l_rate_polynomial_gamma=0.9,
@@ -87,3 +92,11 @@ class SeismicSegmentation(SegmentationModelBase):
         return DatasetSplits.from_proportions(dataset_df, proportion_train=0.8, proportion_val=0.1,
                                               proportion_test=0.1,
                                               random_seed=0)
+    
+    # def get_model_train_test_dataset_splits(self, dataset_df: pd.DataFrame) -> DatasetSplits:
+    #     return DatasetSplits.from_subject_ids(
+    #         df=dataset_df,
+    #         train_ids=['1'],
+    #         val_ids=['2'],
+    #         test_ids=['3'],
+    #     )
